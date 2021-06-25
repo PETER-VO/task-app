167f409035348bf363585707a8c0776e1885ae4a
Phong <v.ducphong@gmail.com>
2021-06-25 10:49:36 +0300

    "add email, password, tokens properties to user model"

--- src/model/user.js
+++ src/model/user.js
@@ -1,10 +1,34 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
         trim: true
+    },
+    password: {
+        type: String,
+        trim: true,
+        minlength: 7,
+        required: true,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error('Password cannot contain password')
+            }
+        }
+    },
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        required: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email not valid')
+            }
+        }
     },
     age: {
         type: Number,
@@ -14,7 +38,13 @@ const userSchema = new mongoose.Schema({
                 throw Error('Age must be positive number')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
 const User = mongoose.model('User', userSchema)
