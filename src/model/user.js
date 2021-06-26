0c34f8034acc61d6639e859bdbd6bcdaa132b864
Phong <v.ducphong@gmail.com>
2021-06-26 12:16:42 +0300

    Create 2 methods for generating JWT and authentication

--- src/model/user.js
+++ src/model/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -47,6 +48,29 @@ const userSchema = new mongoose.Schema({
         }
     }]
 })
+
+userSchema.methods.generateAuthToken = async function(){
+    const user = this
+    const token = jwt.sign({_id: user._id.toString()}, "thisismypassword")
+
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    return token
+}
+
+userSchema.statics.findByCredentials = async function(email, password){
+    const user = await User.findOne({email})
+    if(!user){
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch){
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
 
 userSchema.pre('save', async function(next){
     const user = this
